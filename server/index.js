a4e95a16001ee57cc3503ca2aecfad730a29f149
akhilesh-m01 <bablooakhilesh777@gmail.com>
2024-08-29 20:10:43 -0400

    added url to cors origin

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 4000;
 dbConnect();
 
 app.use(cors({
-    origin: 'http://localhost:5173', // Adjust this based on your needs
+    origin: ['http://localhost:5173','https://blog-app-client1.vercel.app'], // Adjust this based on your needs
     // methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
